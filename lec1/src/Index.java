c9f42ad76cc95db8462619894f325edd011c975e
Stefan <mstp@itu.dk>
2016-10-06 16:28:20 +0200

    Added some documentation to the index interface

--- lec1/src/Index.java
+++ lec1/src/Index.java
@@ -1,11 +1,23 @@
 import java.util.List;
 
+/**
+ *
+ */
 public interface Index {
 
-     void Build(List<Website> websites);
+    /**
+     * This method preprocesses a list of website objects.
+     * @param websites -
+     */
+    void Build(List<Website> websites);
         //prepocesses list of URLs
 
-    List<Website> lookup(String word);
+    /**
+     *
+     * @param query
+     * @return the list of processed websites.
+     */
+    List<Website> lookup(String query);
     //returns list of processed websites
 
 }
