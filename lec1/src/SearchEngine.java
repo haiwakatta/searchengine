3c183ea0701288643c3d5bea55f3a9372997a7a7
Stefan <mstp@itu.dk>
2016-10-10 16:10:59 +0200

    cleanup

--- lec1/src/SearchEngine.java
+++ lec1/src/SearchEngine.java
@@ -67,7 +67,6 @@ public class SearchEngine {
                         System.out.println("Word found on " + w.getUrl());
                     }
                 }
-
             }
 
             // if the method choosen is the Hashmap
