b0e899aafa1fa2a7b5767c008d952b0b3f7a41c4
Stefan <mstp@itu.dk>
2016-11-05 18:01:55 +0100

    Added comment about test∆

--- src/test/java/searchengine/IndexTest.java
+++ src/test/java/searchengine/IndexTest.java
@@ -78,6 +78,8 @@ public class IndexTest {
         lookupTest(invertedIndexTree);
     }
 
+    // Test for whether the invertedIndex creates several lists of websites when the same word is found several times in a website.
+
     // actual code for all the lookups
     private void lookupTest(Index index) {
         List<Website> result = index.lookup("This");
